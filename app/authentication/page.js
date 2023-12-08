350d7171adbb83b1e559a1385ed5b488b5248a3a
WaritTeerapong <warit.teer@kmutt.ac.th>
2023-12-08 17:49:55 +0700

    Update authentication page
    Decoration updates
    - remove border-left
    - increase border radius

--- app/authentication/page.js
+++ app/authentication/page.js
@@ -3,16 +3,14 @@ import businessman from "../../public/images/businessman.png";
 
 export default function Login() {
   return (
-    <div className="font-main text-center mx-56 my-28 border-2 rounded-xl border-black">
+    <div className="font-main text-center mx-64 my-28 border-2 rounded-3xl border-black border-l-0">
         <div className="grid grid-cols-5 grid-row-4 ">
-            <div className="col-span-3 row-span-4 flex flex-col bg-gradient-to-r from-main to-sub border-0 rounded-xl px-8 py-8 text-center">
+            <div className="col-span-3 row-span-4 flex flex-col bg-gradient-to-r from-main to-sub border-0 rounded-2xl px-8 py-8 place-items-center">
                 <Image src={businessman} width={300} height={300}/>
-                <p className>
+                <p className="text-3xl text-white">
+                    <br/>
                     Welcome to our website
                 </p>
-            </div>
-            <div className="col-span-2 row-span-4">
-                02
             </div>
             
 
