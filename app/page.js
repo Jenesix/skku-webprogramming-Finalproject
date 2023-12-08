9653891b36f81c4a868c79bf00b2852f1ec4b5d2
Jenesix <acearcadia789@gmail.com>
2023-12-08 18:42:08 +0900

    finish main content

--- app/page.js
+++ app/page.js
@@ -3,9 +3,9 @@ import hero from "../public/images/hero.png";
 
 export default function Home() {
   return (
-    <div className="font-main text-center">
-      <div className="flex flex-col md:flex-row-reverse justify-evenly bg-gradient-to-r from-main to-sub  text-white w-8 h-auto ">
-           <div className="my-8 md:basics-1/2">
+    <div className="font-main text-center ">
+      <div className="flex flex-col md:flex-row-reverse place-items-center justify-evenly bg-gradient-to-r from-main to-sub  text-white w-8 h-auto ">
+           <div className="my-8 md:ml-0 md:basics-1/2">
                 <Image src={hero} width={300} height={300} />
            </div>
            <div className="my-8 md:basics-1/2 flex flex-col text-left justify-center">
@@ -13,16 +13,16 @@ export default function Home() {
                 Don't stop learning to develop your skills
               </p>
               <p className="text-md">
-                <br></br> 
+                <br/>
                 Learn from expert professionals. Mathematics,
                 Programming, Business, <br></br> and Marketing 
                 to find  
                 your dream. Join Us!
-                <br></br> 
-                <br></br> 
+                <br/> 
+                <br/>
               </p>
 
-              <button className="text-xl py-1 mr-48 bg-white text-main border-2 border-sub rounded-2xl hover:drop-shadow-xl active:bg-sub active:text-white ">
+              <button className="text-xl py-1 md:mr-48 bg-white text-main border-2 border-sub rounded-2xl hover:drop-shadow-xl active:bg-sub active:text-white ">
                 Create your free account
               </button>
            </div>
