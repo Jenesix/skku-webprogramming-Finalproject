f287db38fddd8e0db8ef5a9319bd68b2b03f4508
WaritTeerapong <warit.teer@kmutt.ac.th>
2023-12-08 17:39:59 +0700

    Update authentication page and header
    Authentication page
    - add box
    - add picture
     Header page
    - capitalize the brand name

--- app/authentication/page.js
+++ app/authentication/page.js
@@ -1,10 +1,23 @@
 import Image from "next/image";
-import hero from "../../public/images/hero.png";
+import businessman from "../../public/images/businessman.png";
 
 export default function Login() {
   return (
-    <div className="flex flex-row mx-120 my-120 bg-main">
-        box
+    <div className="font-main text-center mx-56 my-28 border-2 rounded-xl border-black">
+        <div className="grid grid-cols-5 grid-row-4 ">
+            <div className="col-span-3 row-span-4 flex flex-col bg-gradient-to-r from-main to-sub border-0 rounded-xl px-8 py-8 text-center">
+                <Image src={businessman} width={300} height={300}/>
+                <p className>
+                    Welcome to our website
+                </p>
+            </div>
+            <div className="col-span-2 row-span-4">
+                02
+            </div>
+            
+
+        </div>
     </div>
+    
   )
 }
