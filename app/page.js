bf47e58bbeb1bec4645da10b706efd4bb59832a7
WaritTeerapong <warit.teer@kmutt.ac.th>
2023-12-08 15:12:28 +0700

    Banner Update
    - construct responsive flex (flex flex-col md: flex-row)

--- app/page.js
+++ app/page.js
@@ -1,6 +1,26 @@
 export default function Home() {
   return (
-    <div className="font-main text-center mt-4">
+    <div className="font-main text-center">
+      <div className="flex flex-col md:flex-row-reverse justify-evenly bg-gradient-to-r from-main to-sub  text-white w-8 h-auto ">
+           <div className="my-8 md:basics-1/2">
+                pic
+           </div>
+           <div className="my-8 md:basics-1/2 flex flex-col text-left justify-items-start">
+              <p>
+                Don't stop learning to develop your skills
+              </p>
+              <p>
+                Learn from expert professionals. Mathematics, Programming, 
+                
+              </p>
+              <p>
+                Business, and Marketing to find your dream. Join Us!
+              </p>
+              <button className="rounded-lg bg-white text-main border-2">
+                Create your free account
+              </button>
+           </div>
+      </div>
       <h1 className="text-xl">Home</h1>
     </div>
   )
