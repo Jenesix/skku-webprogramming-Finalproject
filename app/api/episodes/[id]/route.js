df71c1f062b87552fec97fd8183f31a024480002
WaritTeerapong <warit.teer@kmutt.ac.th>
2023-12-10 02:36:34 +0700

    Change episode schema
    Change from in_course to ep_course

--- app/api/episodes/[id]/route.js
+++ app/api/episodes/[id]/route.js
@@ -5,9 +5,9 @@ import { NextResponse } from "next/server";
 export async function PUT(request,{params}) {
     try{
         const {id} = params;
-        const {newName: name, new_course: in_course, newDescription: description} = await request.json();
+        const {newName: name, new_course: ep_course, newDescription: description} = await request.json();
         await connectMongoDB();
-        await EP.findByIdAndUpdate(id, {name, in_course, description});
+        await EP.findByIdAndUpdate(id, {name, ep_course, description});
         return NextResponse.json({ message: "EP Updated!" },{ status: 200 }); 
 
     }catch(error){
