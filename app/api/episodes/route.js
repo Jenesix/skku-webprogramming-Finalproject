4e198248046bda4d5b403db1ecf11fa6a6c95261
WaritTeerapong <warit.teer@kmutt.ac.th>
2023-12-09 21:50:29 +0700

    update directory name

---
+++ app/api/episodes/route.js
@@ -0,0 +1,35 @@
+import connectMongoDB from "@/libs/mangodb";
+import EP from "@/models/schema";
+import { NextResponse } from "next/server";
+
+export async function POST(request){
+    try{
+        const {name, description} = await request.json();
+        await connectMongoDB();
+        await EP.create({name, description});
+        return NextResponse.json({message: "EP created!"},{status: 201});
+    } catch (error){
+        console.log('error',error);
+    }
+}
+
+export async function GET() {
+    try{
+        await connectMongoDB();
+        const eps = await EP.find();
+        return NextResponse.json({eps});   
+    } catch(error){
+        console.log('error',error);
+    }
+}
+
+export async function DELETE(request){
+    try{
+        const id = request.nextUrl.searchParams.get("id");
+        await connectMongoDB();
+        await EP.findByIdAndDelete(id);
+        return NextResponse.json({message: "EP Deleted!"},{status: 201});
+    }catch(error){
+        console.log('error',error)
+    }
+}
